040f3b9911843f54e1c84d0ee0b4b25ec25f4ddd
Fede Defaveri <f.g.defaveri@gmail.com>
2019-04-24 03:33:11 +0200

    Renames mockdata to websitedata

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,15 @@
-import mockdata from "../data/mockdata.json";
+import websitesdata from "../data/websitesdata.json";
 import faqdata from "../data/mockfaq.json"
 //TODO check this file's utility and usability in production
 
 const apiFake = {
-    returnFullListData: () => mockdata,
+    returnFullListData: () => websitesdata,
     returnFullFaqData: () => faqdata,
     returnWebsiteData: (websiteName) => { //or (websiteID)
         let found = 0;
         let websiteData = null;
-        for (let i = 0; i < mockdata.length; i++) {
-            mockdata[i].categoryItems.map((item, i) => {
+        for (let i = 0; i < websitesdata.length; i++) {
+            websitesdata[i].categoryItems.map((item, i) => {
                 if (item.name.toLowerCase() === websiteName.toLowerCase()) {
                     websiteData = item;
                 }
