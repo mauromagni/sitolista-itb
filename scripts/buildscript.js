71eb139d2f077ca017700001875d93944c1bd027
Fede Defaveri <f.g.defaveri@gmail.com>
2019-05-07 04:16:55 +0200

    Adds a javascript build script that does pre-build tasks

--- scripts/buildscript.js
+++ scripts/buildscript.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const download = require('image-downloader');
 
 let rawdata = fs.readFileSync('src/data/websitesdata.json');
 
@@ -6,6 +7,18 @@ let websitesdata = JSON.parse(rawdata);
 
 for (let i = 0; i < websitesdata.length ; i++) {
     websitesdata[i].categoryItems.forEach(categoryItem => {
-        console.log(categoryItem.domain)
+        let normalizedName = categoryItem.name.replace(/ /g, "-");
+        const options = {
+            url: `https://www.google.com/s2/favicons?domain=${categoryItem.domain}`,
+            dest: `src/assets/img/favicons/${normalizedName}.png`
+        }
+
+        download.image(options)
+        .then(({ filename, image }) => {
+            console.log('File saved to', filename)
+        })
+        .catch((err) => {
+            console.error(err)
+        })
     });
 }
