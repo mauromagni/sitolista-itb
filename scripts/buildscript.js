d663dd32c5bbfb471392a93308cd581b0744eade
Fede Defaveri <f.g.defaveri@gmail.com>
2019-05-07 04:48:38 +0200

    Updates the prebuild script

--- scripts/buildscript.js
+++ scripts/buildscript.js
@@ -1,13 +1,17 @@
-const fs = require('fs');
-const download = require('image-downloader');
+import fs from 'fs';
+import download from 'image-downloader';
 
-let rawdata = fs.readFileSync('src/data/websitesdata.json');
+//read raw json file and parse it
+const rawdata = fs.readFileSync('src/data/websitesdata.json');
+const websitesdata = JSON.parse(rawdata);  
 
-let websitesdata = JSON.parse(rawdata);  
+//counter variable
+let counter = 0
 
+//loop thru the websites and download their favicons
 for (let i = 0; i < websitesdata.length ; i++) {
     websitesdata[i].categoryItems.forEach(categoryItem => {
-        let normalizedName = categoryItem.name.replace(/ /g, "-");
+        let normalizedName = categoryItem.name.replace(/ /g, "-").toLowerCase();
         const options = {
             url: `https://www.google.com/s2/favicons?domain=${categoryItem.domain}`,
             dest: `src/assets/img/favicons/${normalizedName}.png`
